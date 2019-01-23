948721735d7c31212b9dc365bd3fea8cf63efd8a
Timmo <contact@timmo.xyz>
2019-01-23 13:42:32 +0000

    :hammer: Update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 """Setup configuration."""
 import setuptools
 
-
 with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="repoupdater",
-    version="0.0.23",
-    author="Joakim Sorensen",
-    author_email="ludeeus@gmail.com",
+    version="0.1.0",
+    author="Timmo",
+    author_email="contact@timmo.xyz",
     description="",
     long_description=LONG,
     install_requires=['alpinepkgs', 'click', 'PyGithub>=1.43.4', 'requests'],
     long_description_content_type="text/markdown",
-    url="https://github.com/ludeeus/repoupdater",
+    url="https://github.com/timmo001/repoupdater",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
