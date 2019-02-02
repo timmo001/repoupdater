300d007168a8b3147b6f704e07b0529087014e68
Timmo <contact@timmo.xyz>
2019-02-02 00:21:01 +0000

    :arrow_up: Bump version to v0.2.0 (Due to alpine release)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="repoupdater",
-    version="0.1.5",
+    version="0.2.0",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="",
