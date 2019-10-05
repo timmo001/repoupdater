5479fd3682ae03e6812d87144ac644afa33a81dc
Timmo <contact@timmo.xyz>
2019-10-05 11:10:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="repoupdater",
-    version="0.2.4",
+    version="0.2.5",
     author="Timmo",
     author_email="contact@timmo.xyz",
     description="",
